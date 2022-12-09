5c5afbe59da3f8c829e361e99842e38488bbc111
zehao-wang <zekewang@outlook.com>
2022-12-09 12:00:32 +0100

    mv data extraction to current repo

---
+++ my_local_process/run_sim/run_my_inter_replica.sh
@@ -0,0 +1,3 @@
+scene_path=~/large_data/3D/habitat/scene_datasets/skokloster-castle.glb
+scene_path=~/large_data/3D/replica_v1/apartment_2/habitat/mesh_semantic.ply 
+python run_sim/viewer.py --scene $scene_path --dataset ./run_sim/ui_data/mp3d.scene_dataset_config.json
