422a05073eea648c84f46f8652842110724af410
zehao-wang <zekewang@outlook.com>
2022-12-12 15:19:34 +0100

    fix sem rgb empty has same color to some ins

--- my_local_process/run_sim/viewer.py
+++ my_local_process/run_sim/viewer.py
@@ -26,9 +26,55 @@ from habitat_sim.utils.common import quat_from_angle_axis
 from habitat_sim.utils.settings import default_sim_settings, make_cfg
 from PIL import Image
 from habitat_sim.utils.common import quat_to_magnum
-from habitat_sim.utils.common import d3_40_colors_rgb
 import png
-d3_40_colors_rgb[0] = [0, 0, 0]
+import numpy as np
+
+d3_40_colors_rgb: np.ndarray = np.array(
+    [
+        [1, 1, 1],
+        [31, 119, 180],
+        [174, 199, 232],
+        [255, 127, 14],
+        [255, 187, 120],
+        [44, 160, 44],
+        [152, 223, 138],
+        [214, 39, 40],
+        [255, 152, 150],
+        [148, 103, 189],
+        [197, 176, 213],
+        [140, 86, 75],
+        [196, 156, 148],
+        [227, 119, 194],
+        [247, 182, 210],
+        [127, 127, 127],
+        [199, 199, 199],
+        [188, 189, 34],
+        [219, 219, 141],
+        [23, 190, 207],
+        [158, 218, 229],
+        [57, 59, 121],
+        [82, 84, 163],
+        [107, 110, 207],
+        [156, 158, 222],
+        [99, 121, 57],
+        [140, 162, 82],
+        [181, 207, 107],
+        [206, 219, 156],
+        [140, 109, 49],
+        [189, 158, 57],
+        [231, 186, 82],
+        [231, 203, 148],
+        [132, 60, 57],
+        [173, 73, 74],
+        [214, 97, 107],
+        [231, 150, 156],
+        [123, 65, 115],
+        [165, 81, 148],
+        [206, 109, 189],
+        [222, 158, 214],
+    ],
+    dtype=np.uint8,
+)
 
 RECORD_FREQ = 20
 
@@ -621,16 +667,22 @@ class HabitatSimInteractiveViewer(Application):
         np.save('test_output/sem/%d-label.npy'% total_frames, label_obs)
         np.save('test_output/sem/%d-instance.npy'% total_frames, ins_i_obs)
 
-        label_obs[label_obs==-100] = 0
+        label_obs = label_obs.flatten() 
+        msk = label_obs==-100
+        label_obs = label_obs % 40 + 1
+        label_obs[msk] = 0
         semantic_img = Image.new("P", (semantic_obs.shape[1], semantic_obs.shape[0]))
         semantic_img.putpalette(d3_40_colors_rgb.flatten())
-        semantic_img.putdata((label_obs.flatten() % 40).astype(np.uint8))
+        semantic_img.putdata((label_obs).astype(np.uint8))
         semantic_img.save("test_output/sem/%d-label.png" % total_frames)
-
-        ins_i_obs[ins_i_obs==-100] = 0
+        
+        ins_i_obs = ins_i_obs.flatten() 
+        msk = ins_i_obs==-100
+        ins_i_obs = ins_i_obs % 40 + 1
+        ins_i_obs[msk] = 0
         semantic_img_ins = Image.new("P", (semantic_obs.shape[1], semantic_obs.shape[0]))
         semantic_img_ins.putpalette(d3_40_colors_rgb.flatten())
-        semantic_img_ins.putdata((ins_i_obs.flatten() % 40).astype(np.uint8))
+        semantic_img_ins.putdata((ins_i_obs).astype(np.uint8))
         semantic_img_ins.save("test_output/sem/%d-ins.png" % total_frames)
     
     # def save_semantic_observation_instance(self, obs, total_frames, ignore_idx=-100):
