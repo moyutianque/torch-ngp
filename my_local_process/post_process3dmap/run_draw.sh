f52ba4f0a346ba1700dc0e59ae9653ce2d4a06e3
zehao-wang <zekewang@outlook.com>
2023-01-04 21:59:29 +0100

    del unused scripts

--- my_local_process/post_process3dmap/run_draw.sh
+++
@@ -1,1 +0,0 @@
-python draw_on_plotly.py --bx 50 --tx 500 --bz 50 --tz 450 --by 100 --ty 300 --d_thresh 100
--- run_my_replica_torch_ray.sh
+++
@@ -1,5 +0,0 @@
-scene_path=data/replica_apart2_instance
-python main_nerf.py $scene_path \
---workspace outputs/replica_apart2_instance --fp16 --tcnn --preload \
---tcnn_sem --gui --W 1440 --H 900 --iters 20000 --save_iter 600 --min_near 0. \
---bx -1 --by -1 --bz -1 --tx 1 --ty 1 --tz 1 --num_rays 4096 --distortion_loss #--scale 1 --bound 1 
